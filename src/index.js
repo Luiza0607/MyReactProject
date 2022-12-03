a7ef805bebe9cf62eeb041e66c140b16dfd8c40e
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2022-12-03 17:53:23 +0100

    add props children

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+	<App />,
+	document.querySelector('#root')
+);
